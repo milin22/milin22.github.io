d4e3a593e4a30dd9e010baa3b162cfd87dd02f4a
milin22 <88775159+milin22@users.noreply.github.com>
2022-06-30 08:27:25 +0700

    s

--- VisitCard-Mobile-main/main.js
+++ VisitCard-Mobile-main/main.js
@@ -14,18 +14,4 @@ function changeSocialMediaLinks() {
 }
 changeSocialMediaLinks()
 
-function getGitHubProfileinfos() {
-  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
 
-  fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    userName.textContent = data.name
-    userBio.textContent = data.bio
-    userLink.href = data.html_url
-    userImage.src = data.avatar_url
-    userLogin.textContent = data.login
-  
-  })
-}
-getGitHubProfileinfos()
