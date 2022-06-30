dac26a2d0e9e8ef0d5cc0fe30aecbb04444f9526
milin22 <88775159+milin22@users.noreply.github.com>
2022-06-30 07:00:54 +0700

    test

---
+++ VisitCard-Mobile-main/main.js
@@ -0,0 +1,31 @@
+const LinksSocialMedia = {
+  github: 'daltonbellini',
+  youtube: 'channel/UC1xdCSc7FGo6IfjWGBxAKYw',
+  facebook: '100071669115327',
+  instagram: 'daltonbellini',
+  twitter: 'DevDalton'
+}
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `http://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileinfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+    userImage.src = data.avatar_url
+    userLogin.textContent = data.login
+  
+  })
+}
+getGitHubProfileinfos()
