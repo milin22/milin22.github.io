4e7815caaa3781a35776eba4e249ec3f0dd715c9
milin22 <88775159+milin22@users.noreply.github.com>
2022-09-23 18:09:00 +0700

    portfolio

---
+++ portfolio/assets/js/dynamicTitle.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+  const favicon = document.getElementById("favicon");
+  let pageTitle = document.title;
+  let attentionMessage = "Come back";
+
+  document.addEventListener("visibilitychange", function (e) {
+    let isPageActive = !document.hidden;
+
+    if (!isPageActive) {
+      toggle();
+    } else {
+      document.title = pageTitle;
+    }
+  });
+
+  document.addEventListener("visibilitychange", function (e) {
+    if (!document.hidden) {
+      document.title = pageTitle;
+      favicon.href = "./assets/images/lab.jpg";
+    }
+  });
+
+  function toggle() {
+    if (document.title === attentionMessage) {
+      document.title = pageTitle;
+      favicon.href = "./assets/images/lab.jpg";
+    } else {
+      document.title = attentionMessage;
+      favicon.href = "./assets/images/folded.png";
+    }
+  }
+};
