56072ee414ec7a41656521b365d0290b7fb3e4c8
milin22 <88775159+milin22@users.noreply.github.com>
2022-09-30 23:46:43 +0700

    1

--- portfolio/assets/js/app.js
+++ portfolio/assets/js/app.js
@@ -23,11 +23,10 @@ let header = $(`
 
 <div class="collapse navbar-collapse " id="navbarSupportedContent">
   <ul class="navbar-nav ml-auto" id = "navbar-content">
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Home</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Trang chủ</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Kinh nghiệm</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Sản phẩm</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Học vấn</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
    </li>
@@ -159,7 +158,7 @@ let footer = $(`
     <div class="rounded-social-buttons tag">
    
 
-    <a class="social-button linkedin" href="" target="_blank" >
+    <a class="social-button linkedin" href="https://www.linkedin.com/in/linh-nguy%E1%BB%85n-th%E1%BB%8B-m%E1%BB%B9-413937230/" target="_blank" >
     <!-- svg code for linkedin icon -->
       <svg class="linkedin-icon-footer" xmlns="http://www.w3.org/2000/svg" width="35" viewBox="0 0 24 24" fill="#0e76a8"
         stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
@@ -171,7 +170,7 @@ let footer = $(`
 
 
 
-    <a class="social-button instagram" href="" target="_blank" >
+    <a class="social-button instagram" href="https://www.instagram.com/_ntm.l_/" target="_blank" >
     <!-- svg code for instagram icon -->
       <svg class="instagram-icon-footer" x="0px" y="0px" viewBox="0 0 202.5 202.5"
         style="enable-background:new 0 0 202.5 202.5;">
